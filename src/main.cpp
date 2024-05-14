33bce1b287ab16677e5b0a0eae22a1e3e1aa3385
Yiwen <abbyzhang510@gmail.com>
2024-05-14 13:38:09 -0700

    2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+// #version 120
+// attribute vec4 position;
+// attribute vec4 color;
+// varying vec4 vColor;
+// varying vec4 vColor;
 //========================================================================
 int main( ){
 	ofSetupOpenGL(1280, 1024,OF_WINDOW);			// <-------- setup the GL context
@@ -8,6 +13,9 @@ int main( ){
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
+	// vColor = color;
+    // gl_Position = position;
+	// gl_FragColor = vColor;
 	ofRunApp(new ofApp());
 
 }
